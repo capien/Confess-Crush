3d5d54cd51a0afd2a6440f95b1986424b919f9a7
capien <138313963+capien@users.noreply.github.com>
2023-07-01 22:42:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    title: 'Phải chăng em còn giận anh 🥰',
+    desc: 'Em cho anh điện thoại nói chịn với em nghe ',
+    btnYes: 'Hông còn tí nèo',
+    btnNo: 'Dạ hihi ',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Mình biết mà 🥰. Yêu en nhiều nhiều 😘😘',
+    messDesc: 'Mai ăn ani nha tình yêu hihihih .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
